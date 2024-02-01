23ee35bdcdcbff4f8264f0b1d561b6a06f5fdfe0
LucaCampion <luca212005@gmail.com>
2024-02-01 21:17:18 +0000

    completato ES02

--- www/S2ES02/S2ES02b/ES02b.js
+++ www/S2ES02/S2ES02b/ES02b.js
@@ -10,3 +10,8 @@ let y2 = M*X+Q*1;
 console.log("L'ordinata, con ascissa "+ x +", è: "+ y1);
 console.log("L'ordinata, con ascissa " + X + ", è: "+ y2);
 
+let intersezioneY = Q;
+let intersezioneX = -Q / M;
+
+console.log("Punto di intersezione con l'asse y: "+ intersezioneY);
+console.log("Punto di intersezione con l'asse y: "+ intersezioneX);
---
+++ www/S2ES02/S2ES02c/ES02c.js
@@ -0,0 +1,8 @@
+let studente = {
+    nome: "Mario",
+    cognome: "Rossi",
+    eta: 20
+};
+for (let chiave in studente) {
+    console.log(chiave, studente[chiave]);
+}
